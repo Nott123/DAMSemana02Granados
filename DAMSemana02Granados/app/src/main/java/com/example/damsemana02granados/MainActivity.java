5f60feec9a3f2783930ee5cff02aa5db8f61c9d4
Adrian <lgk9hd.10@gmail.com>
2023-04-17 15:44:32 -0500

    DAMSemana02Granados

---
+++ DAMSemana02Granados/app/src/main/java/com/example/damsemana02granados/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.damsemana02granados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
